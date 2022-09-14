6df337779228126d5bde1efbd2597973e46bef54
mouad hnida <hnidamouad70@gmail.com>
2022-09-14 18:45:04 +0100

    create the server

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('./public'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, console.log(`server listening : ${port}`))
