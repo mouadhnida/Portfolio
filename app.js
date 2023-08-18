b3052204da9ee1de9ac5966ab76c39c299c900b5
Mouad <104643026+mouadhnida@users.noreply.github.com>
2023-08-18 21:15:14 +0100

    fixing some bugs before creating my new portfolio

--- app.js
+++ app.js
@@ -1,46 +1,50 @@
-const path = require('path');
-const express = require('express');
+const path = require("path");
+const express = require("express");
 const app = express();
-require('dotenv').config()
-const nodemailer = require('nodemailer');
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 
+app.use(express.static("./public"));
+app.use(express.json());
 
-app.use(express.static('./public'));
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './index.html'))
-} )
+app.get("/api/form", (req, res) => {
+  res.json({ data: person });
+});
 
-app.get('/api/form', (req,res) => {
-    res.json({ data: person })
-})
-
-app.post('/api/form', (req, res) => {
-    const { personName, personEmail, personMessage } = req.body;
-    if(!personMessage || !personEmail ) {
-        return res.status(404).send('Please provide your email and your message')
-    } res.status(201).json({success: true, name: personName, email: personEmail, message: personMessage})
-    async function main() {
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-              user: process.env.GMAIL_URI,
-              pass: process.env.PASSWORD_URI
-          },
-        });
-        let info = await transporter.sendMail({
-          from: personName,
-          to: "hnidamouad70@gmail.com",
-          subject: `from ${personEmail}`,
-          text: personMessage,
-          
-        });
-        console.log("Message sent: %s", info.messageId);
-        console.log("View email: %s", nodemailer.getTestMessageUrl(info));
-      }
-      main().catch(console.error);
-})
+app.post("/api/form", (req, res) => {
+  const { personName, personEmail, personMessage } = req.body;
+  if (!personMessage || !personEmail) {
+    return res.status(404).send("Please provide your email and your message");
+  }
+  res.status(201).json({
+    success: true,
+    name: personName,
+    email: personEmail,
+    message: personMessage,
+  });
+  async function main() {
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "hnidamouad70@gmail.com",
+        pass: "nxkldkdcojkmxvmd",
+      },
+    });
+    let info = await transporter.sendMail({
+      from: personName,
+      to: "hnidamouad70@gmail.com",
+      subject: `from ${personEmail}`,
+      text: personMessage,
+    });
+    console.log("Message sent: %s", info.messageId);
+    console.log("View email: %s", nodemailer.getTestMessageUrl(info));
+  }
+  main().catch(console.error);
+});
 
 const port = process.env.PORT || 5000;
 
